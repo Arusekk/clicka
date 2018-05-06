2c469391ec63bf62995fe9a8d4387af3657f63b9
Antek55 <ja.wodzinsk@wp.eu>
2018-05-06 20:55:39 +0200

    Rozmówcy w mes według ostatniej wyświetlonej wiadomości

--- xx.py
+++ xx.py
@@ -373,7 +373,7 @@ elif act == "mes":
 	d.setdefault('z', sel_one('select do from messages where od = "{}" group by do order by count(czas) desc limit 1;'.format(username)))
 	
 	print(m['head'], m['body_o'], '<script>z = "{}"</script>'.format(d['z']))
-	rozmowcy = sel_list('select od from messages where do="{u}" group by od order by max(czas) limit 10'.format(u=username))
+	rozmowcy = sel_list('select od from messages where do="{u}" group by od order by max(czas) desc limit 10'.format(u=username))
 	for r in rozmowcy:
 		print(m['entryp_o'].replace('{}', 'mes&z=' + r), imiona[r], '</div></a>')
 
