a222d4868f93fb3c43824306a7e8587f187f3670
Antek <antek@tosia>
2018-05-19 23:06:23 +0200

    Wiadomość do nieokreślonego adresata jest wysyłana do ostatnio nadającego

--- tg.py
+++ tg.py
@@ -29,14 +29,23 @@ def sendmes(tresc):
 if mes == '/start':
     data = {"text": "Wprowadź ten numer w formularzu w menu 'Profil': " + str(o['message']['chat']['id']), 'chat_id': o['message']['chat']['id']}
     requests.post(addr, data=data)
-elif o['message']['text'][0] == '!':
+    exit()
+
+username = sel_one('select username from notifs where tg="{}"'.format(chat_id))
+sid = sel_one('select sid from sessions where username = "{}"'.format(username))
+headers = {'Cookie': 'sid='+sid} 
+if sid == 0:
+    sendmes("Nie jesteś zalogowany do Clicki na żadnym urządzeniu, nie zrobię tego.".format(username))
+    exit()
+
+if o['message']['text'][0] == '!':
     adresat = mes.split(' ', 1)[0][1:]
     tresc = mes.replace('!' + adresat + ' ', '')
     data = {'content': tresc}
-    username = sel_one('select username from notifs where tg="{}"'.format(chat_id))
-    sid = sel_one('select sid from sessions where username = "{}"'.format(username))
-    if sid == 0:
-        sendmes("{}, trzeba być zalogowanym, by móc wysyłać wiadomości.".format(username))
-        exit()
-    headers = {'Cookie': 'sid='+sid} 
+    requests.post('https://anx.nazwa.pl/xx.py?a=mes_b&z={}'.format(adresat), data=data, headers=headers)
+elif mes[0] == '/':
+    pass
+else:
+    adresat = sel_one('select od from messages where do="{}" group by od order by max(czas) desc limit 1'.format(username))
+    data = {'content:' mes}
     requests.post('https://anx.nazwa.pl/xx.py?a=mes_b&z={}'.format(adresat), data=data, headers=headers)
